f59d139f6c97b28b12031dffb0bf9f4ea8f7c550
Daniel Hedegaard <skooter2705@gmail.com>
2026-01-18 20:40:32 +0100

    merge moved to main branch

---
+++ src/Point.java
@@ -0,0 +1,25 @@
+package src;
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // written by Daniel
+    public boolean equals(Point target) {
+        if (this.x == target.x && this.y == target.y) {
+            return true;
+        }
+        return false;
+    }
+
+    // written by Daniel
+    @Override
+    public String toString() {
+        return "x: " + this.x + "  y: " + this.y;
+    }
+}
